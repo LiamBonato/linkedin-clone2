ee5ca4b5c4a64aaa2a0845484fad437ea9d6a08c
LiamBonato <liambonato2000@gmail.com>
2024-02-20 18:33:42 +1100

    Worked on Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         
         { /* Header */ }
-
+          <Header />
         { /* App Body */ }
         { /* Sidebar */ }
         { /* Feed */ }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import HeaderOption from './HeaderOption';
 
 function Header() {
   return (
     <div className='header'>
 
+        <div className='header__left'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/768px-LinkedIn_icon.svg.png' alt=''/>
 
+            <div className='header__search'>
+                <SearchIcon />
+                <input type='text'/>
+            </div>
+        </div>
+
+        <div className='header__right'>
+            <HeaderOption Icon={HomeIcon} title='Home'/>
+            <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+        </div>
 
     </div>
   )
