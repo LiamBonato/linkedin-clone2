60922358e91ebdbfc706354ef4651c3e3853d9ee
LiamBonato <liambonato2000@gmail.com>
2024-02-21 18:08:20 +1100

    Finished icons and avatar

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import HeaderOption from './HeaderOption';
+import { Chat } from '@mui/icons-material';
 
 function Header() {
   return (
@@ -21,6 +25,10 @@ function Header() {
         <div className='header__right'>
             <HeaderOption Icon={HomeIcon} title='Home'/>
             <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+            <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+            <HeaderOption Icon={ChatIcon} title='Messages'/>
+            <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+            <HeaderOption avatar='https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg' title="Me"/>
         </div>
 
     </div>
