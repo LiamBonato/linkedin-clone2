26235b0f4e2aa56c2a45168868eebc95915886e6
LiamBonato <liambonato2000@gmail.com>
2024-02-26 18:26:29 +1100

    Finished Sidebar, added feed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,32 +3,44 @@ import "./Sidebar.css"
 import { Avatar } from '@mui/material'
 
 function Sidebar() {
-  return (
-    <div className='sidebar'>
-        <div className='sidebar__top'>
-            <img src='https://unblast.com/wp-content/uploads/2021/01/Space-Background-Image-2-1024x682.jpg' alt=''/>
-            <Avatar className='sidebar_avatar' />
-            <h2>Yantra</h2>
-            <h4>email@gmail.com</h4>
-        </div>
 
-        <div className='sidebar__stats'>
-            <div className='sider__stat'>
-                <p>Who viewed you</p>
-                <p className='sidebar__statNumber'>2,532</p>
-            </div>
-                
-            <div className='sider__stat'>
-                <p>Views on post</p>
-                <p className='sidebar__statNumber'>4,212</p>
-            </div>
+    const recentItem = (topic) => (
+        <div className='sidebar__recentItem'>
+            <span className='sidebar__hash'>#</span>
+            <p>{topic}</p>
         </div>
+    )
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__top'>
+                <img src='https://unblast.com/wp-content/uploads/2021/01/Space-Background-Image-2-1024x682.jpg' alt=''/>
+                <Avatar className='sidebar_avatar' />
+                <h2>Yantra</h2>
+                <h4>email@gmail.com</h4>
+            </div>
 
-        <div className='sidebar_bottom'>
-            <p>Recent</p>
+            <div className='sidebar__stats'>
+                <div className='sidebar__stat'>
+                    <p>Who viewed you</p>
+                    <p className='sidebar__statNumber'>2,532</p>
+                </div>
+                    
+                <div className='sidebar__stat'>
+                    <p>Views on post</p>
+                    <p className='sidebar__statNumber'>4,212</p>
+                </div>
+            </div>
+
+            <div className='sidebar__bottom'>
+                <p>Recent</p>
+                {recentItem('react.js')}
+                {recentItem('programming.js')}
+                {recentItem('softwareengineering.js')}
+                {recentItem('design.js')}
+                {recentItem('developer.js')}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
